a967d8d7f996aca719c8945824380f0037b257bf
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2023-04-21 11:27:24 +0000

    Added Rules and Regulations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -123,6 +123,36 @@ export default function Home() {
 
       <section className='max-w-6xl mx-auto py-6 pt-8 lg:py-24 lg:pt-0 px-8 lg:px-0' id="useful-information">
         <h2 className='text-4xl 2xl:text-6xl font-bold mb-12 2xl:mb-24'>Useful Information</h2>
+        <div className='w-full mb-24'>
+          <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>Here are some Friendly advice's</h3>
+          <ul className='list-disc'>
+            <li>
+              <p className='text-lg'>
+                Hold <strong>tightly</strong> onto your door during heavy wind, wind damages can be very expensive. 
+              </p>
+            </li>
+            <li>
+              <p className='text-lg'>
+                Make sure to <strong>check</strong> the lid before you pump fuel on the car, our cars are either diesel or gasoline 95. 
+              </p>
+            </li>
+            <li>
+              <p className='text-lg'>
+                Crossing a river is <strong>always</strong> at the renter's responsibility. 
+              </p>
+            </li>
+            <li>
+              <p className='text-lg'>
+                It's <strong>unsafe</strong> and <strong>dangerous</strong> to stop the car on the side of the road
+              </p>
+            </li>
+            <li>
+              <p className='text-lg'>
+                Your mobile phone is <strong>not</strong> a reliable navigation system in Iceland.
+              </p>
+            </li>
+          </ul>
+        </div>
 
         <div className='w-full lg:w-1/2'>
           <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>Insurances are important</h3>
@@ -383,7 +413,7 @@ export default function Home() {
       </section>
 
       <section className="w-full my-6 mt-0 lg:my-24 lg:mt-0" id="driving-in-iceland">
-        <div className="w-full h-[50vh] bg-cover bg-center bg-[url('/cover.jpg')] mb-24">
+        <div className="w-full h-[50vh] bg-cover bg-center bg-[url('/driving-in-iceland-cover.jpg')] mb-24">
           <div className='bg-neutral-900/60 backdrop-brightness-70 h-full w-full flex items-center justify-center'>
             <h2 className='text-4xl 2xl:text-7xl font-bold text-white'>Driving in Iceland</h2>
           </div>
@@ -420,16 +450,164 @@ export default function Home() {
             <img src="https://res.cloudinary.com/car-rentals-is/image/upload/v1663681014/blog/124/16e9fce13c0cc9deaffa8bb1a93ba3cf.jpg" className='w-full lg:w-1/2 h-48 lg:h-80 object-cover' />
           </div>
 
-          <div className='w-full lg:w-1/2 pt-6 lg:pt-12'>
-            <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>What is included in your insurance?</h3>
-            <p className='text-lg'>Read carefully through the terms and conditions of the insurances that you have added to your booking. You can read more about them here: <a href="https://www.geysir.is/terms-and-conditions/" className='link' target="_blank" rel="noopener noreferrer">https://www.geysir.is/terms-and-conditions/</a></p>
+          <div className='w-full'>
+            <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>Sheep in Iceland</h3>
+            <p className='text-lg'>
+              One of the things that you need to be extremely careful with is Iceland's population of over 800,000 wandering sheep. They can be found all along the ring road and they often wander, carelessly, onto the road, right in front of oncoming traffic.
+            </p>
+            <p className='text-lg my-4'>
+            Be especially aware at night, as to the sheep, your car is just a wandering flicker of light and the road is just part of their terrain. Keep your eyes peel at all times.
+            </p>
+            <p className='text-lg'>
+              If an accident occurs farmers in Iceland do appreciate if the local law enforcement are informed so they will be able to attend the wounded animal.
+            </p>
+          </div>
+
+          <div className='w-full'>
+            <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>Roundabouts</h3>
+            <p className='text-lg'>
+              Some tourists get especially confused about roundabouts in Iceland. There are a few roundabouts found around the country, many of them are when entering Reykjavik or other larger towns, like Akureyri.
+            </p>
+            <p className='text-lg my-4'>
+              When you approach a roundabout, make sure you already know which exit you will be taking. You do not indicate when you enter the roundabout, instead, you need to indicate when you are leaving it.
+            </p>
+            <p className='text-lg'>
+              Use the inside line if you plan to exit on the second, third, or fourth exist. Use the outside lane if you are exiting the roundabout on the first exit only. Remember that the inside lane always has the right of way.
+            </p>
+            <p className='text-lg my-4'>
+              If for some reason you've entered the roundabout on the outside lane, but realize you don't need to exit immediately, then use your indicator to signal towards the inside lane - but do not switch lanes! Follow the roundabout on the outside line whilst indicating towards the inside lane until you've reached your exit, at this point indicate towards your exit.
+            </p>
+            <p className='text-lg'>
+              Don't worry, it might sound more stressful than it really is. Also, there are no roundabouts in the whole of Iceland with more than two lanes.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full my-6 mt-0 lg:my-24 lg:mt-0" id="rules-and-regulations">
+        <div className="w-full h-[50vh] bg-cover bg-center bg-[url('/rules-and-regulations-cover.jpg')] mb-24">
+          <div className='bg-neutral-900/60 backdrop-brightness-70 h-full w-full flex items-center justify-center'>
+            <h2 className='text-4xl 2xl:text-7xl font-bold text-white'>Rules & Regulations</h2>
+          </div>
+        </div>
+
+        <div className='max-w-6xl mx-auto py-6 pt-0 lg:py-24 lg:pt-0 space-y-12 px-8 lg:px-0'>
+        <div className='w-full'>
+            <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>Basic rules</h3>
+            <p className='text-lg'>
+              Here are some general rules to have in mind when driving in Iceland.
+            </p>
+
+            <h4 className='mb-4 font-black uppercase mt-8'>The general rules are:</h4>
+            <ul className='list-disc mb-8'>
+              <li className='text-lg'>Traffic drives on the right side of the road.-Wearing a seatbelt at all times is mandatory.</li>
+              <li className='text-lg'>The use of a handheld phone whilst driving is extremely dangerous and forbidden.</li>
+              <li className='text-lg'>It is required to use seatbelts by law, fatal accidents are increasing due to less use. </li>
+              <li className='text-lg'>You are not allowed to drive a vehicle under the influence of alcohol or drugs. </li>
+              <li className='text-lg'>Headlights must be on at all times while driving.</li>
+              <li className='text-lg'>Off-road driving is illegal in Iceland.</li>
+              <li className='text-lg'>Only 4x4 cars are allowed to drive on F roads</li>
+            </ul>
+          </div>
+
+          <div className='w-full'>
+            <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>Speed Limits</h3>
+            <p className='text-lg'>
+              Keeping to the right speed limit is very important as spontaneous dangers can present themselves on Iceland roads. Speeding cameras are dotted around the country and speeding fines can range between ISK 5,000?70,000 (USD 45 to USD 575). Also, please remember that the speed limit is only an indicator of the maximum speed and so if road conditions change (i.e. heavy blizzard) then it's your responsibility to adjust your speed accordingly.
+            </p>
+
+            <h4 className='mb-4 font-black uppercase mt-8'>The general speed limits are:</h4>
+            <ul className='list-disc mb-8'>
+              <li className='text-lg'>90 km/h (56 mph) in the countryside</li>
+              <li className='text-lg'>80 km/h (50 mph) on gravel roads</li>
+              <li className='text-lg'>70 km/h (44 mph) in most road tunnels</li>
+              <li className='text-lg'>50 km/h (31 mph) in urban areas</li>
+              <li className='text-lg'>30 km/h (19 mph) in residential streets</li>
+            </ul>
+
+            <p className='text-lg'>Keep in mind that the above are only general speed limits, some areas may have different conditions so always make sure to check what the signs indicate.</p>
+            
+            <h4 className='mb-4 font-black uppercase mt-8'>Other speeding limits that are important to note:</h4>
+            <ul className='list-disc mb-8'>
+              <li className='text-lg'>Vehicles pulling a trailer (i.e. caravan) are allowed to drive no faster than 90 km/h (56 mph).</li>
+              <li className='text-lg'>Vehicles pulling a trailer that is over 750kg (1,650 lb) and without brakes are not allowed to drive any faster than 90km/h (56 mph).</li>
+            </ul>
+          </div>
+
+          <div className='w-full'>
+            <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>The ring road and F-Roads</h3>
+            <p className='text-lg'>
+              The ring road (e. Road 1) and the F-roads are two completely different roads. The ring road is a paved road that is easy to drive and is suitable for all types of vehicles. The F-roads are unpaved roads that are only suitable for 4x4 vehicles. The F-roads are often impassable long into the summer, the weather conditions can change quickly, you need to be extra careful while driving these roads. F-roads are less maintained than the regular roads, resulting in big rocks on the road or large holes. Bad road condition can cause damages, especially to the underbody of the vehicle. Keep in mind that underbody damages can be <strong>very expensive</strong>.
+            </p>
+
+            <ul className='list-disc my-8'>
+              <li className='text-lg'>The Ring Road (route 1) is mostly paved and easy to drive but some parts of it can be narrow and needs all of your attention. </li>
+              <li className='text-lg'>F-roads are only suitable for AWD or 4WD vehicles and NOT allowed for 2WD cars.</li>
+              <li className='text-lg'>The mountain roads (F-roads) are often impassable long into the summer, the weather conditions can change quickly, you need to be extra careful while driving these roads. </li>
+              <li className='text-lg'>F-roads are less maintained than the regular roads, resulting in big rocks on the road or large holes. Bad road condition can cause damages, especially to the underbody of the vehicle. Underbody damages can be very expensive.</li>
+            </ul>
           </div>
           
-          <div className='w-full lg:w-1/2 ml-auto'>
-            <h3 className='text-2xl 2xl:text-3xl font-bold mb-2 capitalize'>Useful information about our insurances</h3>
-            <p className='text-lg'>
-              First and formost <strong>not</strong> all vehicles are insured on F-Roads. Damages to the underbody of the car are <strong>not</strong> covered with our insurances. Damages caused by sand or ash storms are <strong>only</strong> covered with Gold insurance or Zero Excess. And Windshield and gravel damages are <strong>only</strong> covered with Gold insurance and Zero Excess
-            </p>
+          <div className='w-full'>
+            <h4 className='font-black text-sm uppercase mt-12'>*Here are approximate opening dates of popular F-roads in Iceland:</h4>
+            <table className='w-full table-auto my-4'>
+              <thead>
+                <tr className='bg-neutral-100'>
+                  <th className='p-2 border'>Name</th>
+                  <th className='p-2 border'>Label</th>
+                  <th className='p-2 border'>Opening</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Lakagígar</td>
+                  <td className='border p-2'>F206</td>
+                  <td className='border p-2'>June 15th</td>
+                </tr>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Fjallabaksleið nyrðri (Landmannalaugar, Eldgjá) </td>
+                  <td className='border p-2'>F208</td>
+                  <td className='border p-2'>June 10th</td>
+                </tr>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Landmannaleið, Landmannalaugar</td>
+                  <td className='border p-2'>F225</td>
+                  <td className='border p-2'>June 15th</td>
+                </tr>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Kjölur (Hveravellir)</td>
+                  <td className='border p-2'>F35</td>
+                  <td className='border p-2'>June 10th</td>
+                </tr>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Sprengisandur</td>
+                  <td className='border p-2'>F26</td>
+                  <td className='border p-2'>June 25th</td>
+                </tr>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Askja</td>
+                  <td className='border p-2'>F88</td>
+                  <td className='border p-2'>June 20th</td>
+                </tr>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Kverkfjöll</td>
+                  <td className='border p-2'>F902</td>
+                  <td className='border p-2'>June 20th</td>
+                </tr>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Uxahryggir</td>
+                  <td className='border p-2'>F52</td>
+                  <td className='border p-2'>June 5th</td>
+                </tr>
+                <tr className='even:bg-neutral-100/50 hover:bg-neutral-100'>
+                  <td className='border p-2'>Kaldidalur</td>
+                  <td className='border p-2'>F550</td>
+                  <td className='border p-2'>June 15th</td>
+                </tr>
+              </tbody>
+            </table>
+
+            <p className='italic'>*You should always check the road and weather conditions before traveling to these destinations such as else.</p>
           </div>
         </div>
       </section>
